094e253b2eeb83bc7bb85579cfc3f2578c705c45
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-01-06 21:48:21 +0600

    new input date functionality added

--- Js/main.js
+++ Js/main.js
@@ -1,12 +1,18 @@
-const endDate = new Date(document.querySelector(".countdown").dataset.date);
+const endDate = document.querySelector(".dateInput");
+let endDateElmVAlue = new Date(endDate.value);
 
 const countdownValueElmnt = Array.from(
   document.querySelectorAll(".countdownValue")
 );
 
+endDate.addEventListener("change", () => {
+  endDateElmVAlue = new Date(endDate.value);
+  console.log(endDateElmVAlue);
+});
+
 function countdown() {
   const startDate = new Date();
-  const dateDiff = (endDate - startDate) / 1000;
+  const dateDiff = (endDateElmVAlue - startDate) / 1000;
   if (dateDiff > 0) {
     const days = Math.floor(dateDiff / 3600 / 24);
     const hours = Math.floor(dateDiff / 3600) % 24;
@@ -30,6 +36,7 @@ function countdown() {
     });
   } else {
     clearInterval(setInterval);
+    countdownValueElmnt.map((item) => (item.textContent = "0"));
   }
 }
 setInterval(countdown, 1000);
